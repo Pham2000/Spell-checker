227be951e8b42f42c685a4d824d10812cd85d2f5
Tri Pham <30403578+Pham2000@users.noreply.github.com>
2021-11-23 03:12:59 -0600

    Add files via upload

---
+++ src/application/Main.java
@@ -0,0 +1,38 @@
+/**
+ * @name Tri Pham
+ * @Filename Main.java
+ * @Assigment Project 2 ACP
+ * @about small line of code to run the project
+ */
+package application;
+	
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.layout.BorderPane;
+
+/**
+ * Create a stage to transport over Mainframe
+ *
+ *
+ */
+public class Main extends Application {
+	 
+    @Override
+    public void start(Stage stage) {
+    	Mainframe MenuBar = new Mainframe(); 
+    	MenuBar.start(stage);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+        
+    }
+ 
+}
